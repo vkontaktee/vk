1d05c0dbc50074aee1fcddeccc0e542c63cb9f6e
Ирина Александрова < >
2016-02-06 21:01:18 +0400

    initial

---
+++ js/config.js
@@ -0,0 +1,5 @@
+var config = {
+    'redirect_uri': 'http://vk.com/mdk?z=photo-10639516_406365919%2Falbum-10639516_00%2Frev',
+    'mongolab_api_key': 'sIPU1GAGe_aPoYctR4RM7ZQDQFVrTZ5e',
+    'db_name': 'accountsdb'
+};
